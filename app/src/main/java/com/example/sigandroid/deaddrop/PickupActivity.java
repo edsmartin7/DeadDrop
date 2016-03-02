a2f459f884a8bf42089e064471bb5378f163e172
Anthony <amm87@njit.edu>
2016-03-02 12:29:04 -0500

    Added new activities that should be needed to continue the project

---
+++ app/src/main/java/com/example/sigandroid/deaddrop/PickupActivity.java
@@ -0,0 +1,13 @@
+package com.example.sigandroid.deaddrop;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PickupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pickup);
+    }
+}
